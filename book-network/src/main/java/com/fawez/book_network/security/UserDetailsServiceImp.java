32bedabe161fc67d24e864bf8a1c7e94ae4d0cc3
fawez <tekafawez@gmail.com>
2024-07-12 09:34:42 +0100

    spring security

---
+++ book-network/src/main/java/com/fawez/book_network/security/UserDetailsServiceImp.java
@@ -0,0 +1,21 @@
+package com.fawez.book_network.security;
+
+import com.fawez.book_network.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(()->new UsernameNotFoundException("Email Not Found"));
+    }
+}
---
+++ book-network/src/main/java/com/fawez/book_network/user/Token.java
@@ -0,0 +1,29 @@
+package com.fawez.book_network.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Token {
+     @Id
+    @GeneratedValue
+    private Integer id;
+     private  String value;
+     private LocalDateTime createdAt;
+     private LocalDateTime expiredAt;
+     private LocalDateTime validatedAt;
+     @ManyToOne
+     @JoinColumn(name="userId" ,nullable = false)
+     private User user;
+
+
+}
