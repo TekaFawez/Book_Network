d2570fbed65d84ea5a18fb494f1cde930ca5cc4e
fawez <tekafawez@gmail.com>
2024-08-03 11:16:21 +0100

    menuComponenet

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { ActivateAccountComponent } from './pages/activate-account/activate-acco
 export const routes: Routes = [
     { path:'login',component:LoginComponent},
     {path:'register',component:RegisterComponent},
-    {path:'activate-account',component:ActivateAccountComponent}
+    {path:'activate-account',component:ActivateAccountComponent},
+    {path:'books',loadChildren:()=>import('./modules/book/book.module').then(m=>m.BookModule)}
 ];
